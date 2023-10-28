8715400ec01a189f286b1e0b7bba48cc619eaf5f
Thanakrit Yokart <59278110+butkrit@users.noreply.github.com>
2023-10-28 21:47:33 +0700

    Add files via upload

--- frontend/src/components/topbar/Topbar.jsx
+++ frontend/src/components/topbar/Topbar.jsx
@@ -1,1 +1,25 @@
+import React from 'react'
+import "./topbar.css"
+import SettingsIcon from "@mui/icons-material/Settings";
+import SupervisorAccountIcon from "@mui/icons-material/SupervisorAccount";
 
+
+export default function Topbar() {
+  return (
+    <div className="topbar">
+      <div className="topbarWraper">
+        <div className="topLeft">
+          <span className="logo">SDN Switch</span>
+        </div>
+        <div className="topRight">
+          <div className="topbarIconContainer">
+            <SupervisorAccountIcon />
+          </div>
+          <div className="topbarIconContainer">
+            <SettingsIcon />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
