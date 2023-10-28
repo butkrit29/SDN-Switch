cddc84ec96105d584779b17185e517b9b441e5a0
Thanakrit Yokart <59278110+butkrit@users.noreply.github.com>
2023-10-28 21:46:52 +0700

    Add files via upload

--- frontend/src/components/sidebar/Sidebar.jsx
+++ frontend/src/components/sidebar/Sidebar.jsx
@@ -1,1 +1,21 @@
+import "./sidebar.css"
+import LineStyleIcon from "@mui/icons-material/LineStyle"
+import React from "react"
 
+export default function Sidebar() {
+  return (
+    <div className="sidebar">
+      <div className="sidebarWrapper">
+        <div className="sidebarMenu">
+          <h3 className="sidebarTitle">Dashboard</h3>
+          <ul className="sidebarList">
+            <li className="sidebarListItem">
+              <LineStyleIcon className="sidebarIcon" />
+              Home
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
